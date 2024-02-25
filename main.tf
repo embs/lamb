9db234ddee4a3168adccfdec150b60a4bd95e8ce
Matheus Santana <edmatheus.santana@gmail.com>
2024-02-25 20:33:49 -0300

    terraform: remove unneeded SQS queue

--- main.tf
+++ main.tf
@@ -17,14 +17,6 @@ variable "SECRET_KEY_BASE" {
   type = string
 }
 
-resource "aws_sqs_queue" "rails-lambda-worker" {
-  name                      = "rails-lambda-worker"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-}
-
 resource "aws_ecr_repository" "rails-lambda-worker" {
   name                 = "rails-lambda-worker"
   image_tag_mutability = "MUTABLE"
